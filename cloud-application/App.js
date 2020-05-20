4ac72083b3b7ca128dd62d81af70b82df0b9c31f
Rajesh <s3705430@student.rmit.edu.au>
2020-05-20 15:35:36 +1000

    Refactored

--- cloud-application/App.js
+++ cloud-application/App.js
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
-import Amplify from 'aws-amplify'
-import awsmobile from './amplify/aws-exports'
+import Amplify, { Auth } from "aws-amplify";
+import awsmobile from "./amplify/aws-exports";
 
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from "./navigation/AppNavigator";
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'josefsans-regular': require('./assets/fonts/JosefinSans-Regular.ttf'),
-    'josefsans-thin': require('./assets/fonts/JosefinSans-Thin.ttf'),
-    'josefsans-bold': require('./assets/fonts/JosefinSans-Bold.ttf'),
-    'josefsans-italic': require('./assets/fonts/JosefinSans-Italic.ttf')
-  })
-}
+    "josefsans-regular": require("./assets/fonts/JosefinSans-Regular.ttf"),
+    "josefsans-thin": require("./assets/fonts/JosefinSans-Thin.ttf"),
+    "josefsans-bold": require("./assets/fonts/JosefinSans-Bold.ttf"),
+    "josefsans-italic": require("./assets/fonts/JosefinSans-Italic.ttf"),
+  });
+};
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
-  Amplify.configure(awsmobile)
+
+  Amplify.configure(awsmobile);
+
   if (!fontLoaded) {
     return (
-      <AppLoading 
+      <AppLoading
         startAsync={fetchFonts}
         onFinish={() => setFontLoaded(true)}
       />
-    );      
+    );
   }
 
-  return <AppNavigator/>
+  return <AppNavigator />;
 }
